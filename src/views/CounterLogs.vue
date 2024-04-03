5f69bd133f498e1e6272be601c0c21e64e9ee2d6
Suveyda Kursuncu <suveydakursunc@gmail.com>
2024-04-03 09:24:50 +0300

    feat: added ability to export counter logs as excel file feat: added dropdown to select counter

--- src/views/CounterLogs.vue
+++ src/views/CounterLogs.vue
@@ -1,5 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import * as XLSX from 'xlsx';
+
+onMounted(fetchCounters);
 
 const selectedCategory = ref('gn_value');
 const categories = ref([
@@ -8,18 +12,18 @@ const categories = ref([
 ]);
 
 const months = ref([
-    { name: 'January', code: '1' },
-    { name: 'February', code: '2' },
-    { name: 'March', code: '3' },
-    { name: 'April', code: '4' },
-    { name: 'May', code: '5' },
-    { name: 'June', code: '6' },
-    { name: 'July', code: '7' },
-    { name: 'August', code: '8' },
-    { name: 'September', code: '9' },
-    { name: 'October', code: '10' },
-    { name: 'November', code: '11' },
-    { name: 'December', code: '12' }
+    { name: 'Ocak', code: '1' },
+    { name: 'Şubat', code: '2' },
+    { name: 'Mart', code: '3' },
+    { name: 'Nisan', code: '4' },
+    { name: 'Mayıs', code: '5' },
+    { name: 'Haziran', code: '6' },
+    { name: 'Temmuz', code: '7' },
+    { name: 'Ağustos', code: '8' },
+    { name: 'Eylül', code: '9' },
+    { name: 'Ekim', code: '10' },
+    { name: 'Kasım', code: '11' },
+    { name: 'Aralık', code: '12' }
 ]);
 const currentYear = new Date().getFullYear();
 const yearsList = Array.from({ length: 11 }, (_, index) => ({ name: `${currentYear - index}`, code: `${currentYear - index}` }));
@@ -33,8 +37,12 @@ const endDate = ref('');
 const logs = ref([]);
 const loading = ref(false);
 const error = ref('');
+const exportError = ref('');
 
 const daysInMonth = ref([]);
+
+const counters = ref([]);
+const selectedCounter = ref('');
 
 const updateDate = () => {
     const lastDayOfMonth = new Date(selectedYear.value.code, selectedMonth.value.code, 0).getDate();
@@ -45,9 +53,38 @@ const updateDate = () => {
     daysInMonth.value = Array.from({ length: lastDayOfMonth }, (_, i) => i + 1);
 };
 
-const fetchData = async () => {
+// Function to fetch counters
+async function fetchCounters() {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/v2/counters`, {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${user.token}`
+            }
+        });
+
+        const data = response.data;
+        if (data.success) {
+            counters.value = data.items.map((item) => ({
+                label: item.name,
+                value: item.id
+            }));
+        } else {
+            throw new Error('Request failed');
+        }
+    } catch (error) {
+        console.error('Error fetching counters:', error);
+        // Handle error if needed
+    }
+}
+
+// Function to fetch data for a specific counterId
+const fetchData = async (selectedCounter) => {
     loading.value = true;
     error.value = '';
+    exportError.value = '';
 
     const user = JSON.parse(localStorage.getItem('user'));
     if (!user.token) {
@@ -57,27 +94,40 @@ const fetchData = async () => {
     }
 
     try {
-        const response = await fetch('https://panel.test.app-ges.com/panel/v2/counter-logs/63149d1e3c83a2b2c3fdc7fa', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${user.token}`
+        const response = await axios.post(
+            `https://panel.test.app-ges.com/panel/v2/counter-logs/${selectedCounter}`,
+            {
+                start_date: startDate.value,
+                end_date: endDate.value
             },
-            body: JSON.stringify({ start_date: startDate.value, end_date: endDate.value })
-        });
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${user.token}`
+                }
+            }
+        );
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+        if (!response.data.success) {
+            throw new Error(response.data.message || 'Request failed');
         }
 
-        const responseData = await response.json();
-        logs.value = responseData.data;
+        logs.value = response.data.data;
     } catch (err) {
+        console.error('Error fetching data:', err);
         error.value = err.message;
     } finally {
         loading.value = false;
     }
 };
+
+// Function to handle dropdown change event
+const onSortChange = (selectedCounter) => {
+    fetchData(selectedCounter.value);
+};
+
+// Call fetchCounters function to load counters when the component is mounted
+fetchCounters();
 
 // Modify the getLogValueForHourDay method to display selected value
 const getLogValueForHourDay = (hour, day) => {
@@ -113,72 +163,87 @@ const getTotalForHour = (hour) => {
 
 const getSumForDay = (day) => {
     const logsForDay = logs.value.filter((log) => new Date(log.date).getDate() === day);
-    const sum = logsForDay.reduce((acc, log) => {
+    const totalForDay = logsForDay.reduce((acc, log) => {
         const selectedLogValue = parseFloat(log[selectedCategory.value]);
         return acc + selectedLogValue;
     }, 0);
-    return sum.toFixed(2);
+    return totalForDay.toFixed(2);
 };
 
 const getSumForHour = (hour) => {
-    const logsForHour = logs.value.filter((log) => new Date(log.date).getHours() === hour);
-    const sum = logsForHour.reduce((acc, log) => {
-        const selectedLogValue = parseFloat(log[selectedCategory.value]);
-        return acc + selectedLogValue;
-    }, 0);
-    return sum.toFixed(2);
+    if (hour === 'Total') {
+        const total = logs.value.reduce((acc, log) => {
+            const selectedLogValue = parseFloat(log[selectedCategory.value]);
+            return acc + selectedLogValue;
+        }, 0);
+        return total.toFixed(2);
+    }
+};
+
+const exportToExcel = () => {
+    if (logs.value.length === 0) {
+        exportError.value = 'Veri henüz alınmadı. Lütfen önce veriyi alın.';
+        return;
+    }
+
+    const worksheet = XLSX.utils.table_to_sheet(document.querySelector('table'));
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    XLSX.writeFile(workbook, 'tablo_data.xlsx');
 };
 </script>
 
 <template>
-    <div class="pt-3 px-3 bg-white border-2 border-gray-100 mb-3">
+    <div class="p-3 bg-white border-2 border-gray-100 mb-3">
         <div>
-            <div class="flex gap-2">
-                <Dropdown id="dropdown" v-model="selectedMonth" :options="months" optionLabel="name" placeholder="Select Month" @change="updateDate" class="mb-5" />
-                <Dropdown id="dropdown" v-model="selectedYear" :options="years" optionLabel="name" placeholder="Select Year" @change="updateDate" class="mb-5" />
-            </div>
-
-            <div class="grid">
-                <div class="col-12 md:col-4">
-                    <div class="field-radiobutton mb-0 gap-3">
-                        <div v-for="category in categories" :key="category.key" class="flex align-items-center">
-                            <RadioButton v-model="selectedCategory" :inputId="category.key" name="dynamic" :value="category.key" />
-                            <label :for="category.key" class="ml-2">{{ category.name }}</label>
+            <div class="toolbar">
+                <Dropdown id="dropdown" v-model="selectedCounter" :options="counters" optionLabel="label" placeholder="Sayaçlar" class="mb-5" v-show="counters.length" />
+                <Dropdown id="dropdown" v-model="selectedMonth" :options="months" optionLabel="name" placeholder="Ay Seçin" @change="updateDate" />
+                <Dropdown id="dropdown" v-model="selectedYear" :options="years" optionLabel="name" placeholder="Yıl Seçin" @change="updateDate" />
+                <div class="grid">
+                    <div class="col-12 md:col-4">
+                        <div class="field-radiobutton mb-0 gap-3">
+                            <div v-for="category in categories" :key="category.key" class="flex align-items-center">
+                                <RadioButton v-model="selectedCategory" :inputId="category.key" name="dynamic" :value="category.key" />
+                                <label :for="category.key" class="ml-2">{{ category.name }}</label>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <Button @click="onSortChange(selectedCounter)" label="Sayaç Okuma Verilerini Al"></Button>
+                <Button @click="exportToExcel" label="Tabloyu Dışa Aktar"></Button>
             </div>
-
-            <button @click="fetchData">Fetch Data</button>
         </div>
 
+        <div v-if="exportError">{{ exportError }}</div>
         <div v-if="loading">Loading...</div>
         <div v-else-if="error">Error: {{ error }}</div>
         <div v-else>
             <div v-if="logs.length > 0" class="overflow-x-auto mt-4">
-                <table class="table-auto min-w-full border-collapse border border-black">
+                <table class="table-auto min-w-full" style="border-collapse: collapse; border: 1px solid #dee2e6">
                     <thead>
                         <tr>
-                            <th class="px-4 py-2 border-1">Saat/Gün</th>
-                            <th v-for="day in daysInMonth" :key="day" class="px-4 py-2 border-1">{{ day }}</th>
-                            <th class="px-4 py-2 border-1">Toplam</th>
+                            <th class="px-4 py-2">Saat/Gün</th>
+                            <th v-for="day in daysInMonth" :key="day" class="px-4 py-2" style="border-collapse: collapse; border: 1px solid #dee2e6">{{ day }}</th>
+                            <th class="px-4 py-2">Toplam</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody style="border-collapse: collapse; border: 1px solid #dee2e6">
                         <tr
                             v-for="hour in Array(24)
                                 .fill(null)
                                 .map((_, idx) => idx.toString().padStart(2, '0'))"
                             :key="hour"
+                            style="border-collapse: collapse; border: 1px solid #dee2e6"
                         >
-                            <td class="px-4 py-2 border-1">{{ hour }}</td>
-                            <td v-for="day in daysInMonth" :key="day" class="px-4 py-2 border-1">{{ getLogValueForHourDay(hour, day) }}</td>
-                            <td class="px-4 py-2 border-1">{{ getTotalForHour(hour) }}</td>
+                            <td class="px-4 py-2">{{ hour }}</td>
+                            <td v-for="day in daysInMonth" :key="day" class="px-4 py-2" style="border-collapse: collapse; border: 1px solid #dee2e6">{{ getLogValueForHourDay(hour, day) }}</td>
+                            <td class="px-4 py-2">{{ getTotalForHour(hour) }}</td>
                         </tr>
                         <tr>
-                            <td class="px-4 py-2 border-1">Toplam</td>
-                            <td v-for="day in daysInMonth" :key="day" class="px-4 py-2 border-1">{{ getSumForDay(day) }}</td>
-                            <td class="px-4 py-2 border-1">{{ getSumForHour('Total') }}</td>
+                            <td class="px-4 py-2">Toplam</td>
+                            <td v-for="day in daysInMonth" :key="day" class="px-4 py-2" style="border-collapse: collapse; border: 1px solid #dee2e6">{{ getSumForDay(day) }}</td>
+                            <td class="px-4 py-2">{{ getSumForHour('Total') }}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -186,3 +251,14 @@ const getSumForHour = (hour) => {
         </div>
     </div>
 </template>
+
+<style scoped>
+.toolbar {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    align-items: baseline;
+    flex-wrap: wrap;
+    margin-bottom: 1rem;
+}
+</style>
